86b51657991ed433bf2f0ffc3218fcb0a8dd47eb
Akhil KR <rakhil@wallpostdev.com>
2024-02-15 00:55:02 +0530

    gg

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,23 @@ $('#students').owlCarousel({
         }
     }
 })
+$('#Popular').owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 1,
+            nav: true
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: true
+        }
+    }
+})
